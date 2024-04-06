ee3efda7a91517d24b0041d67869415f782f9885
Uriah <codeitlikemiley@gmail.com>
2024-04-06 23:58:17 +0800

    update config

--- src/config.rs
+++ src/config.rs
@@ -1,44 +1,95 @@
-use serde::Deserialize;
 use std::collections::HashMap;
 
-#[derive(Debug, Deserialize, Default)]
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize, Clone, Default)]
 pub struct Config {
-    #[serde(default)]
     pub commands: Commands,
 }
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Commands {
-    pub run: Option<CommandConfig>,
-    pub test: Option<CommandConfig>,
-    pub build: Option<CommandConfig>,
+    pub run: CommandConfig,
+    pub test: CommandConfig,
+    pub build: CommandConfig,
 }
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct CommandConfig {
-    pub default: Option<String>,
-    #[serde(default)]
+    pub default: String,
     pub configs: HashMap<String, CommandDetails>,
 }
 
-#[derive(Debug, Deserialize, Default)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct CommandDetails {
-    #[serde(rename = "type", default)]
-    pub command_type: Option<String>,
-    #[serde(default)]
-    pub command: Option<String>,
-    #[serde(default)]
-    pub params: Option<String>,
-    #[serde(default)]
+    #[serde(rename = "type")]
+    pub command_type: String,
+    pub command: String,
+    pub params: String,
     pub env: HashMap<String, String>,
-    #[serde(default = "default_bool_false")]
     pub allow_multiple_instances: bool,
-    #[serde(default)]
-    pub working_directory: Option<String>,
-    #[serde(default)]
-    pub pre_command: Option<String>,
+    pub working_directory: String,
+    pub pre_command: String,
 }
 
-fn default_bool_false() -> bool {
-    false
+impl Default for Commands {
+    fn default() -> Self {
+        let mut run_configs = HashMap::new();
+        run_configs.insert(
+            "default".into(),
+            CommandDetails {
+                command_type: "cargo".into(),
+                command: "run".into(),
+                params: "".into(),
+                env: HashMap::new(),
+                allow_multiple_instances: false,
+                working_directory: ".".into(),
+                pre_command: "".into(),
+            },
+        );
+
+        let mut test_configs = HashMap::new();
+        test_configs.insert(
+            "default".into(),
+            CommandDetails {
+                command_type: "cargo".into(),
+                command: "test".into(),
+                params: "".into(),
+                env: HashMap::new(),
+                allow_multiple_instances: true,
+                working_directory: ".".into(),
+                pre_command: "".into(),
+            },
+        );
+
+        let mut build_configs = HashMap::new();
+        build_configs.insert(
+            "default".into(),
+            CommandDetails {
+                command_type: "cargo".into(),
+                command: "build".into(),
+                params: "".into(),
+                env: HashMap::new(),
+                allow_multiple_instances: false,
+                working_directory: ".".into(),
+                pre_command: "".into(),
+            },
+        );
+
+        Commands {
+            run: CommandConfig {
+                default: "standard".into(),
+                configs: run_configs,
+            },
+            test: CommandConfig {
+                default: "default".into(),
+                configs: test_configs,
+            },
+            build: CommandConfig {
+                default: "debug".into(),
+                configs: build_configs,
+            },
+        }
+    }
 }
+
