ba1035ce153f1a2275291bc3d553689530d3a1d7
Uriah <codeitlikemiley@gmail.com>
2024-04-07 09:31:22 +0800

    added global using once_cell

--- src/global.rs
+++ src/global.rs
@@ -1,6 +1,15 @@
 use lazy_static::lazy_static;
+use once_cell::sync::OnceCell;
 use std::sync::{Arc, Mutex};
 
+use crate::config::CommandConfig;
+
 lazy_static! {
-    pub static ref APP_CONFIG: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
+    pub static ref CONFIGURATION_FILE: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
 }
+
+pub static DEFAULT_RUN_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
+pub static DEFAULT_TEST_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
+pub static DEFAULT_BUILD_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
+pub static DEFAULT_BENCH_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
+pub static DEFAULT_SCRIPT_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
