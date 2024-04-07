63f9b6aa3467143fc1222f1652faa3a0945a60b5
Uriah <codeitlikemiley@gmail.com>
2024-04-07 09:32:33 +0800

    main working

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,27 @@
-use std::error::Error;
+use std::{error::Error, path::Path};
 
-use rx::{config::Config, global::APP_CONFIG, helper::read_file};
+use rx::{
+    config::{CommandContext, Config},
+    global::CONFIGURATION_FILE,
+    helper::{init_default_config, read_file},
+};
 
 fn main() -> Result<(), Box<dyn Error>> {
+    init_default_config();
     // Assume you have this filename from somewhere
-    let filename = "rx.toml";
+    let filename = Path::new("rx.toml");
     read_file(filename)?;
 
     // Lock the Mutex and get a reference to the String, don't try to move it
-    let file_content = APP_CONFIG.lock().unwrap();
+    let file_content = CONFIGURATION_FILE.lock().unwrap();
 
-    let config: Config = toml::from_str(&file_content)?;
+    let config: Config = toml::from_str(&file_content).unwrap_or(Config::default());
 
-    //println!("{:#?}", config.commands.run.configs.get("leptos").unwrap());
-    //println!("{:#?}", config.commands.run.configs.get("dioxus").unwrap());
-    //println!("{:#?}", config.commands.run.configs.get("default").unwrap());
-
-    let config2: Config = Config::default();
     println!(
         "{:#?}",
-        config2.commands.run.configs.get("default").unwrap()
-    );
-    println!(
-        "{:#?}",
-        config2.commands.test.configs.get("default").unwrap()
-    );
-    println!(
-        "{:#?}",
-        config2.commands.build.configs.get("default").unwrap()
+        config
+            .commands
+            .get_command_config(CommandContext::Run, "leptos")
     );
 
     Ok(())
