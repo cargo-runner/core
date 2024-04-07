9c15290fc366199416f7c59f578e47746f507fa5
Uriah <codeitlikemiley@gmail.com>
2024-04-07 13:10:00 +0800

    added example for set_default_config by CommandContext

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     run_config.update_config(config_key.to_string(), command_details);
 
     // If needed to remove, you can directly call it without checking for existence
-    // run_config.remove_config(config_key);
+    //run_config.remove_config(config_key);
+
+    config
+        .commands
+        .set_default_config(CommandContext::Run, "leptos")?;
 
     // Save the updated configuration
     config.save(Some(PathBuf::from("rx.toml")))?;
