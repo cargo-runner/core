886e0d3d8305947f9f624dd8dbd1bd7b85269a91
Uriah <codeitlikemiley@gmail.com>
2024-04-07 10:26:30 +0800

    use PathBuf instead of String

--- src/global.rs
+++ src/global.rs
@@ -1,13 +1,18 @@
 use lazy_static::lazy_static;
 use once_cell::sync::OnceCell;
-use std::sync::{Arc, Mutex};
+use std::{
+    path::PathBuf,
+    sync::{Arc, Mutex},
+};
 
 use crate::config::CommandConfig;
 
 lazy_static! {
-    pub static ref CONFIGURATION_FILE: Arc<Mutex<String>> = Arc::new(Mutex::new(String::new()));
+    pub static ref CONFIGURATION_FILE_CONTENT: Arc<Mutex<String>> =
+        Arc::new(Mutex::new(String::new()));
 }
 
+pub static DEFAULT_CONFIG_PATH: OnceCell<PathBuf> = OnceCell::new();
 pub static DEFAULT_RUN_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
 pub static DEFAULT_TEST_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
 pub static DEFAULT_BUILD_CONFIG: OnceCell<CommandConfig> = OnceCell::new();
