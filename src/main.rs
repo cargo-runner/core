5cb089b1409a84610b61b4f27588f70cf2bd668f
Uriah <codeitlikemiley@gmail.com>
2024-04-07 10:28:30 +0800

    add a way to override loading of default config

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,18 @@ use std::{error::Error, path::Path};
 
 use rx::{
     config::{CommandContext, Config},
-    helper::init_default_config,
+    helper::init_config,
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
-    init_default_config();
-    let filename = Path::new("rx.toml");
+    // note this should be from the env::args()
+    let default_config_path = Path::new("rx.toml");
 
-    let config: Config = Config::load(filename)?;
+    init_config(default_config_path);
+
+    // we can override Config or load different config by passing in a Path
+    // If we Passed None we use the DEFAULT_CONFIG_PATH
+    let config: Config = Config::load(None)?;
 
     println!(
         "{:#?}",
