92afe83690fe92b4f9fc122e78450c199196fe43
Uriah <codeitlikemiley@gmail.com>
2024-04-07 09:56:48 +0800

    use Config::load(path)

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,14 @@ use std::{error::Error, path::Path};
 
 use rx::{
     config::{CommandContext, Config},
-    global::CONFIGURATION_FILE,
-    helper::{init_default_config, read_file},
+    helper::init_default_config,
 };
 
 fn main() -> Result<(), Box<dyn Error>> {
     init_default_config();
-    // Assume you have this filename from somewhere
     let filename = Path::new("rx.toml");
-    read_file(filename)?;
 
-    // Lock the Mutex and get a reference to the String, don't try to move it
-    let file_content = CONFIGURATION_FILE.lock().unwrap();
-
-    let config: Config = toml::from_str(&file_content).unwrap_or(Config::default());
+    let config: Config = Config::load(filename)?;
 
     println!(
         "{:#?}",
