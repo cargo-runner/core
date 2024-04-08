51e6206d4155413cf8e97b9608e292c50a5d18d8
Uriah <codeitlikemiley@gmail.com>
2024-04-08 22:33:11 +0800

    added helpers for initializing default config path

--- src/helper.rs
+++ src/helper.rs
@@ -5,8 +5,10 @@ use std::{
     path::{Path, PathBuf},
 };
 
+use dirs::home_dir;
+
 use crate::{
-    config::CommandConfig,
+    config::{CommandConfig, Config},
     global::{
         CONFIGURATION_FILE_CONTENT, DEFAULT_BENCH_CONFIG, DEFAULT_BUILD_CONFIG,
         DEFAULT_CONFIG_PATH, DEFAULT_RUN_CONFIG, DEFAULT_SCRIPT_CONFIG, DEFAULT_TEST_CONFIG,
@@ -71,4 +73,25 @@ pub fn is_valid_env_var_name(name: &str) -> bool {
 
     // Check the rest of the characters
     chars.all(|c| c.is_ascii_uppercase() || c.is_ascii_digit() || c == '_')
+}
+
+pub fn default_config_path() -> PathBuf {
+    home_dir()
+        .expect("Could not find home directory")
+        .join(".config/cargo_runner/config.toml")
+}
+pub fn ensure_config_directory_and_file(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    if !path.exists() {
+        if let Some(parent) = path.parent() {
+            std::fs::create_dir_all(parent).expect("Failed to create configuration directory");
+        }
+        create_default_config_file(path)?;
+    }
+    Ok(())
+}
+pub fn create_default_config_file(path: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let default_config = Config::default();
+    let toml = toml::to_string(&default_config)?;
+    write_to_config_file(path, &toml)?;
+    Ok(())
 }
