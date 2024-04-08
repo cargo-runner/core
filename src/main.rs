9c06f0f545b046ad6863e3ee655825f031aa6c1b
Uriah <codeitlikemiley@gmail.com>
2024-04-08 20:30:44 +0800

    organize code structure for main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::{BTreeSet, HashMap},
+    error::Error,
     path::PathBuf,
 };
 
@@ -12,17 +13,11 @@ use rx::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let path_str = "rx.toml";
+    let (config_path, config_key, command, params, env, pre_commands) = fetch_params()?;
 
-    let default_config_path = PathBuf::from(path_str);
+    init_config(config_path.clone());
 
-    init_config(default_config_path.clone());
-
-    let mut config: Config = Config::load(Some(default_config_path.clone()))?;
-
-    let pre_commands: BTreeSet<String> = ["default"].into_iter().map(String::from).collect();
-
-    let config_key = "leptos";
+    let mut config: Config = Config::load(Some(config_path.clone()))?;
 
     let valid_pre_command_keys = config.commands.get_configs(CommandContext::Run);
 
@@ -41,7 +36,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 )));
             }
         }
-
         Ok(())
     });
 
@@ -54,6 +48,48 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         Ok(())
     });
 
+    let run_command_details = CommandDetailsBuilder::new(CommandType::Cargo, command)
+        .pre_command(pre_commands)
+        .env(env)
+        .params(params)
+        .add_validator(pre_command_validator)
+        .add_validator(env_validator)
+        .build()?;
+
+    let run_config = config.commands.get_or_default_config(CommandContext::Run);
+
+    run_config.update_config(config_key, run_command_details);
+
+    config
+        .commands
+        .set_default_config(CommandContext::Run, config_key)?;
+
+    config.save(Some(config_path))?;
+
+    Ok(())
+}
+
+fn fetch_params<'a>() -> Result<
+    (
+        PathBuf,
+        &'a str,
+        &'a str,
+        &'a str,
+        HashMap<String, String>,
+        BTreeSet<String>,
+    ),
+    Box<dyn Error>,
+> {
+    let config_path = PathBuf::from(
+        std::env::args()
+            .nth(1)
+            .unwrap_or_else(|| "rx.toml".to_string()),
+    );
+    let pre_commands: BTreeSet<String> = ["default"].into_iter().map(String::from).collect();
+
+    let config_key = "leptos";
+    let params = "watch";
+
     let command = "leptos";
     let env: HashMap<String, String> = [
         ("APP_NAME", "Cargo Runner"),
@@ -63,26 +99,5 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     .iter()
     .map(|(k, v)| (String::from(*k), String::from(*v)))
     .collect();
-
-    let run_command_details = CommandDetailsBuilder::new(CommandType::Cargo, command)
-        .command("leptos")
-        .pre_command(pre_commands)
-        .env(env)
-        .params("-- this is the last watch")
-        .add_validator(pre_command_validator)
-        .add_validator(env_validator)
-        .build()?;
-
-    let run_config = config.commands.get_or_default_config(CommandContext::Run);
-
-    run_config.update_config(config_key, run_command_details);
-    // If setting the newly created config as default then set_default_config
-    // should be invoke after update_config for that CommandDetailsBuilder was set
-    config
-        .commands
-        .set_default_config(CommandContext::Run, config_key)?;
-
-    config.save(Some(default_config_path))?;
-
-    Ok(())
+    Ok((config_path, config_key, command, params, env, pre_commands))
 }
