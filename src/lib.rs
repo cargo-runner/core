02f599dad0cf8413826b90ce5dc1b532f500118a
Uriah <codeitlikemiley@gmail.com>
2024-04-16 05:32:01 +0800

    delete types

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,4 @@ pub mod errors;
 pub mod global;
 pub mod helpers;
 pub mod models;
-pub mod types;
 pub mod validator;
--- src/types.rs
+++ src/types.rs
@@ -1,4 +1,1 @@
-use crate::{errors::ConfigError, models::config::CommandDetails};
 
-pub type CommandDetailsValidation =
-    Box<dyn Fn(&CommandDetails) -> Result<(), ConfigError> + 'static>;
